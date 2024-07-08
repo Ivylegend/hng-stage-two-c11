55cbd45b0c832d8d60f24651bc1f565a882a762e
Ivylegend <victoryicha2@gmail.com>
2024-07-08 10:02:23 +0100

    swap images

--- app/page.tsx
+++ app/page.tsx
@@ -96,8 +96,8 @@ export default function Home() {
             MORE CATEGORIES TO EXPLORE
           </h2>
           <div className="flex overflow-x-auto gap-5 md:gap-0 md:flex-wrap justify-between">
-            <ImageCard name="suit jackets" image="/assets/image2.png" />
             <ImageCard name="jumpsuit" image="/assets/image11.png" />
+            <ImageCard name="suit jackets" image="/assets/image2.png" />
             <ImageCard name="loungewear" image="/assets/image12.png" />
             <ImageCard name="sweats" image="/assets/image5.png" />
           </div>
